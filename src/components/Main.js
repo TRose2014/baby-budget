c104443c1e3593f5b68fe7edec9bcab245135730
TRose2014 <shortyshort789@aol.com>
2022-09-13 17:24:11 +0900

    removed comments

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,19 +4,12 @@ import Home from './Home/Home';
 
 function Main() {
   return (
-    <div>
+    <main>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/item" element={<ItemList />} />
       </Routes>
-    </div>
-    // <main>
-    //   <Routes>
-    //     <Route exact path="/" component={Home} />
-    //     <Route path="/item" component={itemList} />
-    //     {/* <Route path="/drinks" component={() => (<ItemCard data={data[0]} />)} /> */}
-    //   </Routes>
-    // </main>
+    </main>
   );
 }
 export default Main;
