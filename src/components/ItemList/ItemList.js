fe899065e936041a187e8d48f15e514887a8ac27
TRose2014 <shortyshort789@aol.com>
2022-10-05 15:52:52 +0900

    have hard coded todo list and ability to add new items working

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,5 +1,36 @@
+import React, {useState} from 'react';
+import {TextField , Button } from '@mui/material';
+
+
 const ItemList = () => {
-  return <h1>Item List</h1>;
+  const [todos,setTodos] = useState([
+    'Create Blockchain App',
+    'Create a Youtube Tutorial'
+    ]);
+    const [input, setInput]=useState('');
+
+    const addTodo=(e)=>{
+      e.preventDefault();
+      setTodos([...todos,input]);
+      setInput('');
+    };
+
+    return (
+      <div>
+        <h2> TODO List App</h2>
+        <form>
+          <TextField id="outlined-basic" label="Make Todo" variant="outlined" style={{margin:"0px 5px"}} size="small" value={input}
+          onChange={e=>setInput(e.target.value)} />
+          <Button variant="contained" color="primary" onClick={addTodo}  >Add Todo</Button>
+        </form>
+        <ul>
+        {todos.map(todo => <li>{todo}</li>)}
+        </ul>
+      </div>
+
+    )
+  
+  // return <h1>Item List</h1>;
 };
 
 export default ItemList;
